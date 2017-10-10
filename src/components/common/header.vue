2702b613e80c99ab465bee42e43bbb9ddefae459
iu-peng <761322559@qq.com>
2017-10-10 23:50:56 +0800

    canvasclock

--- src/components/common/header.vue
+++ src/components/common/header.vue
@@ -1,5 +1,5 @@
 <template>
-	<div>
+	<div id="header">
 		<el-menu 
 			theme="dark" 
 			:default-active="activeIndex" 
@@ -10,24 +10,29 @@
 				<i class="iconfont icon-bar-chart-h"></i>
 			</el-menu-item>
 			<el-menu-item index="2">处理中心</el-menu-item>
-			<el-submenu index="3">
-				<template slot="title">我的工作台</template>
-				<el-menu-item index="2-1">选项1</el-menu-item>
-				<el-menu-item index="2-2">选项2</el-menu-item>
-				<el-menu-item index="2-3">选项3</el-menu-item>
-			</el-submenu>
-		<el-menu-item index="4">订单管理</el-menu-item>
-	</el-menu>
+				<el-submenu index="3">
+					<template slot="title">我的工作台</template>
+					<el-menu-item index="2-1">选项1</el-menu-item>
+					<el-menu-item index="2-2">选项2</el-menu-item>
+					<el-menu-item index="2-3">选项3</el-menu-item>
+				</el-submenu>
+			<el-menu-item index="4">订单管理</el-menu-item>
+		</el-menu>
+		<canvas-clock class="canvas"></canvas-clock>
 	</div>
 	
 </template>
 
 <script>
+	import canvasClock from '@/components/page/canvasclock/canvas'
 	export default {
 		data(){
 			return {
 				activeIndex:'1'
 			}
+		},
+		components:{
+			canvasClock
 		},
 		methods:{
 			changeCollapse(){
@@ -37,11 +42,19 @@
 		}
 	}
 </script>
-<style>
+<style scoped>
+	#header{
+		width:100%;
+		position:relative;
+	}
 	.el-menu-demo{
 		padding-left:12.5%;
-	}
-	.el-menu{
 		border-radius:0;
+	}
+	.canvas{
+		position:absolute;
+		right:0;
+		top:0;
+		padding:15px 20px;
 	}
 </style>
